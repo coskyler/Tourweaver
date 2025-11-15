c69467b58f4c58e2f7f25795b747cde04197fb0d
Skyler Quinby <skylerpquinby@gmail.com>
2025-11-15 03:03:50 -0500

    Update gemini prompts

--- Backend/src/jobs/generate_description.js
+++ Backend/src/jobs/generate_description.js
@@ -7,9 +7,11 @@ Location = "${city}"
 User request = "${userPrompt}"
 Tour = "${tour}"
 
-Write a short description of the tour with an emphasis on the tour locations.
-Use a positive, energetic tone that matches the tour.
-Respond in plain text, and do not include any other information besides the tour description.
+Write a natural, short description of the tour with an emphasis on the tour locations.
+Use a light tone that matches the tour and adds a hint anticipation.
+ONLY IF THE TOUR IS AN EMPTY LIST: Briefly explain directly to the user why their request wasn’t clear enough to generate a tour. Do not ask follow-up questions, simply state why it was too vague to generate a tour.
+Ensure that you do not sound unnaturally excited or overly positive.
+Respond in plain text, and do not include any formatting or any information besides the tour description.
 `;
 
   const res = await ai.models.generateContent({
--- Backend/src/jobs/generate_name.js
+++ Backend/src/jobs/generate_name.js
@@ -8,6 +8,7 @@ User request = "${userPrompt}"
 
 Quickly state a short name for their tour.
 Respond in plain text, and do not include any other information besides the tour name. Do not add a period at the end.
+If you can not reasonably decide a tour name from the request, state a generic tour name and do not think.
 `;
 
   const res = await ai.models.generateContent({
